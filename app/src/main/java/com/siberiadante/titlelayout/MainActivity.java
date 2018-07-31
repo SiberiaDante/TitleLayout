5ab51feceff56febc8e1bdc629a5bcb0ced51aa6
SiberiaDante <994537867@qq.com>
2018-07-31 17:35:15 +0800

    v1.1.1

--- app/src/main/java/com/siberiadante/titlelayout/MainActivity.java
+++ app/src/main/java/com/siberiadante/titlelayout/MainActivity.java
@@ -1,5 +1,6 @@
 package com.siberiadante.titlelayout;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,5 +38,6 @@ public class MainActivity extends AppCompatActivity {
 
         final TitleBarLayout titleBarLayout = (TitleBarLayout) findViewById(R.id.title_layout_four);
         titleBarLayout.setIsLeftBackView(true);
+        titleBarLayout.setTitleLayoutBackground(ContextCompat.getColor(this,R.color.colorPrimaryDark));
     }
 }
